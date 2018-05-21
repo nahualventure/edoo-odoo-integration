9ea13a2292947ce4d44b35f576f9bdd5955ae8cd
danielucas <a_lejandro27@hotmail.com>
2018-05-21 10:19:09 -0600

    add edoo_api_integration

--- odoo/api.py
+++ odoo/api.py
@@ -125,8 +125,30 @@ def get_allowed_invoice_journals():
 def get_allowed_payment_journals():
     return settings.ODOO_SETTINGS['ALLOWED_PAYMENT_JOURNALS']
 
-
 def get_account_statement(client_id, comercial_id, filters):
+    url, db, username, password = get_odoo_settings()
+    comercial_id = int(comercial_id)
+    client_id = int(client_id)
+
+    allowed_invoice_journals = get_allowed_invoice_journals()
+    allowed_payment_journals = get_allowed_payment_journals()
+
+    uid = services.authenticate_user(url, db, username, password)
+
+    models = xmlrpclib.ServerProxy('{}/xmlrpc/2/object'.format(url))
+
+    transactions_by_company = models.execute_kw(db, uid, password,
+            'api.integration', 'get_account_statement',
+                [{'comercial_id': comercial_id,
+                'client_id': comercial_id,
+                'allowed_invoice_journals': allowed_invoice_journals,
+                'allowed_payment_journals': allowed_payment_journals,
+                'filters': filters}]
+        )
+
+    return transactions_by_company
+
+def get_account_statement_legacy(client_id, comercial_id, filters):
     url, db, username, password = get_odoo_settings()
     comercial_id = int(comercial_id)
     client_id = int(client_id)
