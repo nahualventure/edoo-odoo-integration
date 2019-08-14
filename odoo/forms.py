4b0ee230b6730f1f6157ccac6a4a39a7232fcbe3
danlgz <danielalejandro137@gmail.com>
2019-08-14 08:50:45 -0600

    required vat field

--- odoo/forms.py
+++ odoo/forms.py
@@ -32,7 +32,7 @@ class PaymentResponsableConfigurationForm(forms.Form):
     client_ref = forms.CharField(required=False, label=_(u"Vacío para autogenerar"))
     comercial_id = forms.IntegerField(required=False, widget=forms.HiddenInput, initial=None)
     comercial_name = forms.CharField(required=True, label=_(u"ie. Juan López"))
-    comercial_number = forms.CharField(required=False, label=_(u"ie. 11111111-1"))
+    comercial_number = forms.CharField(required=True, label=_(u"ie. 11111111-1"))
     comercial_address = forms.CharField(required=True, label=_(u"ie. Ciudad"))
     comercial_email = forms.EmailField(required=True, label=_(u"ie. correo@email.com"))
 
