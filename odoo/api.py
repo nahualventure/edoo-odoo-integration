588b758dadb5e2b952d6c52c3e208ce7587c5288
Daniel Lucas <danielalejandro137@gmail.com>
2020-07-15 11:42:35 -0600

    FIX library

--- odoo/api.py
+++ odoo/api.py
@@ -299,7 +299,7 @@ def get_odoo_company():
     url, db, username, password = get_odoo_settings()
 
     uid = services.authenticate_user(url, db, username, password)
-    models = ServerProxy('{}/xmlrpc/2/object'.format(url))
+    models = xmlrpclib.ServerProxy('{}/xmlrpc/2/object'.format(url))
 
     company_id = Odoo.CUSTOM_SETTINGS['company_pk']
 
